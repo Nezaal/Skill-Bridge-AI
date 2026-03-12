246001641a077e1c972f88388481900a9790de78
nezaal <ahmadnezal19@gmail.com>
2026-03-12 17:33:48 +0530

    feat: cors handeling and hook layer

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
+const cors = require('cors')
+
 
 const app = express()
 
@@ -7,6 +9,11 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
 // require all routes here
 const authRouter = require("./routes/auth.routes")
 
