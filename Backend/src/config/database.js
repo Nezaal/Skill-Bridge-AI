5f90740dd182c7ac7c39dadf7145bac68beeca21
nezaal <ahmadnezal19@gmail.com>
2026-05-03 11:26:26 +0530

    fix: database err handling

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,16 +1,38 @@
 const mongoose = require("mongoose")
 
+const MONGODB_URI = process.env.MONGODB_URI;
 
+IF(!MONGODB_URI){
+    throw new Error('pleas define the mongodb uri')
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, Promise: null };
+}
 async function connectToDB() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-
-        console.log("connected to db")
-
-
-    }catch(err){
-        console.log(err);
+    if (cached.conn) {
+        return cached.conn;
     }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+            return mongoose;
+        });
+    }
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
+    return cached.conn;
 }
 
 module.exports = connectToDB
