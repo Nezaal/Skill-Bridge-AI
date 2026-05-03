1846f57db1d1649fba01e1e42d6941d6458afb92
nezaal <ahmadnezal19@gmail.com>
2026-05-03 15:23:22 +0530

    fix: port

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,17 @@
 require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
+const PORT = process.env.PORT || 5000;
 
 
 connectToDB()
 
 
 
-app.listen(3000, () => {
-    console.log("server running on port 3000")
-})
+// Adding '0.0.0.0' explicitly tells Express to accept connections from outside the container
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 
 // generateInterviewReport({ resume, selfDescription, jobDescription })
