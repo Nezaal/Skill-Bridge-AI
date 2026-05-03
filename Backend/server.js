373e8851ab4c5ce3f6b20358e9423a24a91127a1
nezaal <ahmadnezal19@gmail.com>
2026-05-03 20:47:48 +0530

    fix: cors error it was some how in the frontend instead of backend lol

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,21 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 const PORT = process.env.PORT || 5000;
+const cors = require('cors'); 
 
 
 connectToDB()
 
+
+
+// Allow requests from your local frontend AND your deployed frontend URL
+app.use(cors({
+    origin: [
+        'http://localhost:5173', // Vite default local port
+        'https://skill-bridge-ai-production.up.railway.app' // Add your production frontend URL here when you deploy it
+    ],
+    credentials: true, 
+}));
 
 
 // Adding '0.0.0.0' explicitly tells Express to accept connections from outside the container
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,21 +4,6 @@ import { AuthProvider } from "./features/auth/auth.context"
 import { InterviewProvider } from "./features/interview/interview.context"
 
 
-const cors = require('cors'); 
-
-
-
-// Allow requests from your local frontend AND your deployed frontend URL
-app.use(cors({
-    origin: [
-        'http://localhost:5173', // Vite default local port
-        'https://skill-bridge-ai-production.up.railway.app' // Add your production frontend URL here when you deploy it
-    ],
-    credentials: true, 
-}));
-
-
-
 function App() {
 
 
